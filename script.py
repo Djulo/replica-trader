b3640e1b28ba3aaf50a765e34b423c5abbf400e9
Nikola Kovacevic <nkovacevic@microsoft.com>
2022-11-02 20:17:26 +0100

    Improve the script.

--- script.py
+++ script.py
@@ -3,9 +3,7 @@ from config import replica, account
 import time
 import pprint
 
-exchange_id = 'ftx'
-exchange_class = getattr(ccxt, exchange_id)
-replica = exchange_class({
+replica = ccxt.ftx({
     'apiKey': replica['apiKey'],
     'secret': replica['secret'],
     'headers': {
@@ -13,7 +11,7 @@ replica = exchange_class({
     }
 })
 
-account = exchange_class({
+account = ccxt.ftx({
     'apiKey': account['apiKey'],
     'secret': account['secret'],
     'headers': {
@@ -21,23 +19,33 @@ account = exchange_class({
     }
 })
 
-account_orders = replica.fetchOrders()
+since = time.time() * 1000 - 1000 * 60 * 60
+
+orders = replica.fetch_orders()
+account_orders = [x for x in orders if x['timestamp'] < since]
 
 while True:
-    replica_balance = replica.fetchBalance()['USD']['free']
-    account_balance = account.fetchBalance()['USD']['free']
-    factor = replica_balance / account_balance
+    try:
+        replica_balance = replica.fetch_balance()['USD']['free']
+        account_balance = account.fetch_balance()['USD']['free']
+        factor = replica_balance / account_balance
 
-    replica_orders = replica.fetchOrders()
-    orders = [x for x in replica_orders if x not in account_orders]
+        orders = replica.fetch_orders(since=since)
 
-    for order in orders:
-        try:
+        orders_to_create = [x for x in orders if x not in account_orders]
+        orders_to_create = replica.sort_by(orders_to_create, 'timestamp')
+        if len(orders_to_create) > 0:
+            since = orders_to_create[-1]['timestamp']
+
+        for order in orders:
             amount = order['amount'] / factor
-            order = account.createOrder(order['symbol'], order['type'], order['side'], amount, ...)
-            pprint(order)
-        except:
-            pass
+            order = account.create_order(order['symbol'], order['type'], order['side'], amount, ...)
 
-    account_orders = replica_orders
-    time.sleep(2)
+        account_orders.extend(orders_to_create)
+        time.sleep(2)
+    except ccxt.NetworkError as e:
+        print(e)  # retry on next iteration
+    except ccxt.ExchangeError as e:
+        print(e)
+    except Exception as e:
+        print(e)
