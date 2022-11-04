e76547e5ea63946729d167da94f97279a38dc76e
Nikola Kovacevic <nkovacevic@microsoft.com>
2022-11-04 12:01:14 +0100

    Change to async execution.

--- script.py
+++ script.py
@@ -1,58 +1,70 @@
-import ccxt
-from config import replica, account
-import time
-import pprint
+# import ccxt
+from config import replica_config, account_config
+import ccxt.pro as ccxtpro
+from asyncio import run
 
-replica = ccxt.ftx({
-    'apiKey': replica['apiKey'],
-    'secret': replica['secret'],
-    'headers': {
-        'FTX-SUBACCOUNT': replica['subaccount']
-    }
-})
 
-account = ccxt.ftx({
-    'apiKey': account['apiKey'],
-    'secret': account['secret'],
-    'headers': {
-        'FTX-SUBACCOUNT': account['subaccount']
-    }
-})
-
-since = None
-account_orders = replica.fetch_orders()
-
-while True:
+async def place_order(replica, account, order):
     try:
-        orders = replica.fetch_orders(since=since)
-        orders_to_create = [x for x in orders if x not in account_orders]
-        orders_to_create = replica.sort_by(orders_to_create, 'timestamp')
+        balance = await replica.fetch_balance()
+        replica_balance = balance['USD']['free']
 
-        for order in orders_to_create:
-            replica_balance = replica.fetch_balance()['USD']['free']
-            account_balance = account.fetch_balance()['USD']['free']
+        balance = await account.fetch_balance()
+        account_balance = balance['USD']['free']
 
-            factor = replica_balance / account_balance
-            amount = order['amount'] / factor
+        factor = replica_balance / account_balance
+        amount = order['amount'] / factor
 
-            account.load_markets()
-            market = account.market(order['symbol'])
+        await account.load_markets()
+        market = account.market(order['symbol'])
 
-            if amount < market['limits']['amount']['min']:
-                amount = market['limits']['amount']['min']
+        if amount < market['limits']['amount']['min']:
+            amount = market['limits']['amount']['min']
 
-            position = next((x for x in replica.fetch_positions() if x['symbol'] == order['symbol']), None)
-            if position is not None and position['info']['size'] == order['amount']:
-                position = next((x for x in account.fetch_positions() if x['symbol'] == order['symbol']), None)
-                if position is not None:
-                    amount = position['info']['size']
+        positions = await replica.fetch_positions()
+        replica_position = next((x for x in positions if x['symbol'] == order['symbol'] and x['contracts'] > 0), None)
+        positions = await account.fetch_positions()
+        account_position = next((x for x in positions if x['symbol'] == order['symbol'] and x['contracts'] > 0), None)
 
-            res = account.create_order(order['symbol'], order['type'], order['side'], amount, ...)
-            since = order['timestamp']
-            account_orders.append(order)
-    except ccxt.NetworkError as e:
-        print(e)  # retry on next iteration
-    except ccxt.ExchangeError as e:
-        print(e)
+        if (replica_position is not None and
+            account_position is not None and
+            replica_position['contracts'] == order['amount']) or \
+                (replica_position is None and
+                 account_position is not None):
+            amount = account_position['contracts']
+        elif replica_position is None and \
+                account_position is None:
+            return
+
+        await account.create_order(order['symbol'], order['type'], order['side'], amount, ...)
     except Exception as e:
         print(e)
+
+
+async def watch_orders(replica, account):
+    while True:
+        try:
+            orders = await replica.watch_orders()
+            for order in orders:
+                await place_order(replica, account, order)
+        except Exception as e:
+            print(e)
+            break
+
+
+replica = ccxtpro.ftx({
+    'apiKey': replica_config['apiKey'],
+    'secret': replica_config['secret'],
+    'headers': {
+        'FTX-SUBACCOUNT': replica_config['subaccount']
+    }
+})
+account = ccxtpro.ftx({
+    'apiKey': account_config['apiKey'],
+    'secret': account_config['secret'],
+    'headers': {
+        'FTX-SUBACCOUNT': account_config['subaccount']
+    }
+})
+
+run(watch_orders(replica, account))
